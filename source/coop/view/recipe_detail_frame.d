8fad12a7fe551b4eb116d39c794cd58d91c97fda
もじょんぬ <moe.e.coop@gmail.com>
2016-01-26 03:17:05 +0900

    複数のバインダーで同じ名前のレシピを所有していた時の詳細表示を修正

--- source/coop/view/recipe_detail_frame.d
+++ source/coop/view/recipe_detail_frame.d
@@ -139,6 +139,8 @@ class RecipeDetailFrame: ScrollWidget
                      .map!(k => tuple(k,
                                       make!(RedBlackTree!dstring)(ret.binders.filter!(b => chars[k].hasRecipe(r.name, b)).array)))
                      .assocArray;
+        import std.stdio;
+        writeln("OWNERS: ", ret.owners);
         return ret;
     }
 
@@ -213,11 +215,13 @@ class RecipeDetailFrame: ScrollWidget
     {
         owners_ = os;
         auto bLen = binders.length;
+        import std.stdio;
+        writeln("binders", bLen);
         childById("owners").text =
             os.keys.empty
             ? "なし"
             : os.keys.map!((k) {
-                    if (bLen)
+                    if (bLen == 1)
                     {
                         return k;
                     }

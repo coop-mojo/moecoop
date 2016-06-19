6b4315b238df9effc12cdff6e58dddfee0e0613c
もじょんぬ <moe.e.coop@gmail.com>
2016-06-19 19:34:22 +0900

    リリース用のファイルに libcurl.dll が含まれていなかったのを修正

--- archive.bat
+++ archive.bat
@@ -1,3 +1,3 @@
 @echo off
 dub build -a x86_64 -b release
-7za a -tzip moecoop.zip fukuro.exe LICENSE README.md resource doc libfreetype-6.dll
+7za a -tzip moecoop.zip fukuro.exe LICENSE README.md resource doc libcurl.dll

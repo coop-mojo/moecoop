0d80e1d135365a4ec0f1989c3bfe1c04a0d6d9d1
もじょんぬ <moe.e.coop@gmail.com>
2016-06-23 02:30:45 +0900

    アーカイブ用のコマンドを AppVeyor でも動くように修正

--- archive.bat
+++ archive.bat
@@ -1,3 +1,3 @@
 @echo off
 dub build -a x86_64 -b release
-7za a -tzip moecoop.zip fukuro.exe LICENSE README.md resource doc libcurl.dll
+7z a -tzip moecoop.zip fukuro.exe LICENSE README.md resource doc libcurl.dll

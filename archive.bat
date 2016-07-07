e1b55188b514ae88ae07d012da06ad6789007cd0
もじょんぬ <moe.e.coop@gmail.com>
2016-07-08 04:50:33 +0900

    7z の代わりに powershell でアーカイブを作るように変更

--- archive.bat
+++ archive.bat
@@ -1,3 +1,3 @@
 @echo off
 dub build -a x86_64 -b release
-7z a -tzip moecoop.zip fukuro.exe LICENSE README.md resource doc libcurl.dll
+powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, doc, libcurl.dll -DestinationPath moecoop.zip

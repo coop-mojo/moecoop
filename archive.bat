869561252e63cf46e6d715f8c2219573c987dc46
もじょんぬ <moe.e.coop@gmail.com>
2016-07-15 22:03:11 +0900

    Windows 32bit 版のバイナリも生成するようにした

--- archive.bat
+++ archive.bat
@@ -1,3 +1,6 @@
 @echo off
 dub build -a x86_64 -b release
-powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, doc, libcurl.dll -DestinationPath moecoop.zip
+powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, doc, libcurl.dll -DestinationPath moecoop-64bit.zip
+
+dub build -a x86 -b release
+powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, doc, libcurl.dll -DestinationPath moecoop-32bit.zip

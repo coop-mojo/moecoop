1b05b51e18cedff9f373679f409354df36ba466d
もじょんぬ <moe.e.coop@gmail.com>
2016-07-22 01:38:12 +0900

    AppVeyor のテストを32bit版と64bit版に分離

--- archive.bat
+++ archive.bat
@@ -1,6 +1,3 @@
 @echo off
-dub build -a x86_64 -b release
-powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, doc, libcurl.dll -DestinationPath moecoop-64bit.zip
-
-dub build -a x86 -b release
-powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, doc, libcurl.dll -DestinationPath moecoop-32bit.zip
+dub build -a %1 -b release
+powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, doc, libcurl.dll -DestinationPath moecoop-%1.zip

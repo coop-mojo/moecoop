340f64a01d3accfbc9bf54f66a91a80973764fff
もじょんぬ <moe.e.coop@gmail.com>
2016-08-07 04:28:04 +0900

    アーカイブ対象を修正

--- archive.bat
+++ archive.bat
@@ -1,3 +1,3 @@
 @echo off
 dub build -a %1 -b release
-powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, doc, libcurl.dll -DestinationPath moecoop-%1.zip
+powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs, libcurl.dll -DestinationPath moecoop-%1.zip

0613aa2e056bb997fc31f65ac520121ba29273ec
もじょんぬ <moe.e.coop@gmail.com>
2016-08-09 21:24:08 +0900

    [skip travis] AppVeyor でデバッグビルド版も取得できるようにした

--- archive.bat
+++ archive.bat
@@ -1,3 +1,3 @@
 @echo off
-dub build -a %1 -b release
+dub build -a %1 -b %2
 powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs, libcurl.dll -DestinationPath moecoop-%1.zip

bc280beea6c890ab99e0bcd0d9bdc38bf4c1898a
もじょんぬ <moe.e.coop@gmail.com>
2016-10-05 01:05:59 +0900

    スキるぽんの URL を解釈するモジュールを追加

---
+++ source/coop/model/skills.d
@@ -0,0 +1,108 @@
+/**
+ * Copyright: Copyright (c) 2016 Mojo
+ * Authors: Mojo
+ * License: $(LINK2 https://github.com/coop-mojo/moecoop/blob/master/LICENSE, MIT License)
+ */
+module coop.model.skills;
+
+enum SkillPon = "http://www.ponz-web.com/skill/";
+
+/**
+ * スキルシミュレーターの URL を読み込んで、キャラクターの種族とスキル値を返す
+ */
+auto parseSimulatorURL(string url)
+{
+    import std.algorithm;
+    import std.exception;
+
+    if (url.startsWith(SkillPon))
+    {
+        import std.array;
+        import std.concurrency;
+        import std.conv;
+        import std.regex;
+        import std.typecons;
+
+        enum Races = [
+            "Invalid", "ニューター", "コグニート", "エルモニー", "パンデモス",
+            ];
+        enum Skills = [
+            "Invalid", "筋力", "着こなし", "攻撃回避", "生命力",
+            "知能", "持久力", "精神力", "集中力", "呪文抵抗力",
+            "素手", "刀剣", "こんぼう", "槍", "銃器", "弓", "盾",
+            "投げ", "牙", "罠", "キック", "戦闘技術", "酩酊",
+            "物まね", "調教", "破壊魔法", "回復魔法", "強化魔法",
+            "神秘魔法", "召喚魔法", "死の魔法", "魔法熟練", "自然調和",
+            "暗黒命令", "取引", "シャウト", "音楽", "盗み", "ギャンブル",
+            "パフォーマンス", "ダンス", "落下耐性", "水泳", "死体回収",
+            "包帯", "自然回復", "採掘", "伐採", "収穫", "釣り",
+            "解読", "料理", "鍛冶", "醸造", "木工", "裁縫", "薬調合",
+            "装飾細工", "複製", "栽培", "美容",
+            ];
+
+        auto baseStr = url.dup;
+        enforce(baseStr.skipOver(SkillPon));
+
+        // 種族読み込み
+        auto matcher = baseStr.matchFirst(ctRegex!r"^?(\d+)&");
+        enforce(!matcher.empty);
+        auto race = Races[matcher[1].to!int];
+
+        // スキル値読み込み
+        auto app = appender!(Tuple!(int, string)[]);
+        auto str = matcher.post;
+        while(!str.empty)
+        {
+            auto m = str.matchFirst(ctRegex!r"^(\d{1,2})([a-zA-F]{2})");
+            enforce(!m.empty);
+            app.put(tuple(m[1].to!int, m[2].assumeUnique));
+            str = m.post;
+        }
+        return tuple(race,
+                     app.data
+                        .map!(tpl =>
+                              tuple(Skills[tpl[0]],
+                                    tpl[1].ponHexToInt/10.0))
+                        .assocArray);
+    }
+    else
+    {
+        enforce(false, "未サポートのサイトです！");
+    }
+    assert(false);
+}
+
+///
+unittest
+{
+    auto url = "http://www.ponz-web.com/skill/?3&1jm4gi6sy28se29cA34ew48Ce49Ce51Fi53Fi55Fi56Fi60ym";
+    auto ret = parseSimulatorURL(url);
+    assert(ret[0] == "エルモニー");
+    assert(ret[1] == [
+               "筋力": 30.0, "生命力": 20.0, "持久力": 60.0,
+               "収穫": 90.0, "釣り": 90.0, "料理": 100.0,
+               "醸造": 100.0, "裁縫": 100.0, "薬調合": 100.0,
+               "美容": 78.0, "神秘魔法": 58.0, "召喚魔法": 9.0,
+               "取引": 15.0]);
+}
+
+/**
+ * スキるぽんでスキル値の使われている文字列を整数値に変換する。
+ * 有効な数字は a-zA-F で、それぞれ32進数の 0-9a-v に対応している。
+ */
+@property auto ponHexToInt(string str) @safe
+{
+    import std.conv;
+    import std.regex;
+    return str.replaceAll!(s => cast(char[])[s.hit[0]-'a'+'0'])(ctRegex!"[a-j]")
+              .replaceAll!(s => cast(char[])[s.hit[0]-'k'+'a'])(ctRegex!"[k-z]")
+              .replaceAll!(s => cast(char[])[s.hit[0]-'A'+'q'])(ctRegex!"[A-F]")
+              .to!int(32);
+}
+
+///
+@safe unittest
+{
+    assert("jm".ponHexToInt == 300);
+    assert("Fi".ponHexToInt == 1000);
+}

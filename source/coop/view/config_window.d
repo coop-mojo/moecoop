28dae1d0aa7698e225912e643e98258e34a075bb
もじょんぬ <moe.e.coop@gmail.com>
2016-10-07 00:40:08 +0900

    設定画面の各ボタンの機能はおおよそ実装

--- source/coop/view/config_window.d
+++ source/coop/view/config_window.d
@@ -7,7 +7,6 @@ module coop.view.config_window;
 
 import dlangui;
 import dlangui.dialogs.dialog;
-import dlangui.dialogs.filedlg;
 
 import coop.model.config;
 import coop.model.character;
@@ -31,12 +30,6 @@ class ConfigDialog: Dialog
 
     override void initialize()
     {
-        version(Windows) {
-            enum directory = "フォルダ";
-        } else {
-            enum directory = "ディレクトリ";
-        }
-
         auto wLayout = parseML(q{
                 VerticalLayout {
                     padding: 10
@@ -84,34 +77,69 @@ class ConfigDialog: Dialog
         StringListAdapter charList = new StringListAdapter;
         chars_.keys.each!(c => charList.add(c));
         wLayout.childById!ListWidget("characters").ownAdapter = charList;
+        wLayout.childById!ListWidget("characters").selectedItemIndex = 0;
         dstring baseDir = chars_.values.front.baseDirectory;
 
+        if (chars_.keys.length == 1)
+        {
+            wLayout.childById("deleteCharacter").enabled = false;
+        }
+
         wLayout.childById("addCharacter").click = (Widget src) {
-            /// ダイアログ開く
-            // キャラクター名
-            // スキルシミュレーターURL
-            // スキるぽんで編集/(spacer)/OK/キャンセル/
-            auto newChar = wLayout.childById("toBeAdded").text;
-            chars_[newChar] = new Character(newChar, baseDir);
-            charList.add(newChar);
-            auto mainWidget = _parentWindow.mainWidget;
-            with(mainWidget)
-            {
-                childById!RecipeTabFrame("binderFrame").characters = chars_.keys;
-                childById!RecipeTabFrame("skillFrame").characters = chars_.keys;
-                childById!RecipeMaterialTabFrame("materialFrame").characters = chars_.keys;
-            }
+            auto dlg = new CharacterSettingDialog(window, chars_);
+            dlg.dialogResult = (Dialog dlg, const Action action) {
+                if (action && action.id == StandardAction.Ok)
+                {
+                    import std.exception;
+                    auto cdlg = cast(CharacterSettingDialog)dlg;
+                    auto name = enforce(cdlg.charNameBox.text);
+                    auto url = cdlg.urlBox.text;
+                    chars_[name] = new Character(name, baseDir);
+                    // url
+                    charList.add(name);
+                    auto mainWidget = _parentWindow.mainWidget;
+                    with(mainWidget)
+                    {
+                        childById!RecipeTabFrame("binderFrame").characters = chars_.keys;
+                        childById!RecipeTabFrame("skillFrame").characters = chars_.keys;
+                        childById!RecipeMaterialTabFrame("materialFrame").characters = chars_.keys;
+                    }
+                    wLayout.childById("deleteCharacter").enabled = true;
+                }
+            };
+            dlg.show;
             return true;
         };
+
+        wLayout.childById("editCharacter").click = (Widget src) {
+            auto list = wLayout.childById!ListWidget("characters");
+            auto charIdx = list.selectedItemIndex;
+            /// Out of Index の場合は？
+            auto charName = charList.items[charIdx].to!dstring;
+            auto ch = chars_[charName];
+            // auto url = ch.url;
+
+            auto dlg = new CharacterSettingDialog(window, chars_, charName/*, url*/);
+            dlg.dialogResult = (Dialog dlg, const Action action) {
+                if (action && action.id == StandardAction.Ok)
+                {
+                    auto url = (cast(CharacterSettingDialog)dlg).urlBox.text;
+                    // ch.url = url;
+                }
+            };
+            dlg.show;
+            return true;
+        };
+
         wLayout.childById("deleteCharacter").click = (Widget src) {
             auto list = wLayout.childById!ListWidget("characters");
             auto deletedIdx = list.selectedItemIndex;
+            /// Out of Index の場合は？
             auto deleted = charList.items[deletedIdx];
             auto c = chars_[deleted];
             chars_.remove(deleted);
             c.deleteConfig;
-            charList.clear;
-            chars_.keys.each!(c => charList.add(c));
+            charList.remove(deletedIdx);
             auto mainWidget = _parentWindow.mainWidget;
             with(mainWidget)
             {
@@ -119,6 +147,11 @@ class ConfigDialog: Dialog
                 childById!RecipeTabFrame("skillFrame").characters = chars_.keys;
                 childById!RecipeMaterialTabFrame("materialFrame").characters = chars_.keys;
             }
+            if (chars_.keys.length == 1)
+            {
+                src.enabled = false;
+            }
+            // どこかのタイミングで selectedItemIndex を設定する必要あり
             return true;
         };
 
@@ -150,11 +183,8 @@ class ConfigDialog: Dialog
         {
             wLayout.childById("migemoInstaller").click = (Widget src) {
                 wLayout.childById("migemoInstaller").enabled = false;
-                childById("button-action1").enabled = false;
-                scope(exit) childById("button-action1").enabled = true;
-
-                childById("button-action2").enabled = false;
-                scope(exit) childById("button-action2").enabled = true;
+                childById("close-button").enabled = false;
+                scope(exit) childById("close-button").enabled = true;
 
                 wLayout.childById("migemoInstaller").text = "インストール中...";
                 scope(success) wLayout.childById("migemoInstaller").text = "インストール済み";
@@ -164,7 +194,6 @@ class ConfigDialog: Dialog
             };
         }
 
-        wLayout.backgroundColor = 0xCCCCCC;
         addChild(wLayout);
 
         auto exits = new HorizontalLayout;
@@ -173,6 +202,7 @@ class ConfigDialog: Dialog
         spacer.layoutWidth(FILL_PARENT);
         exits.addChild(spacer);
         auto close = new Button(ACTION_CLOSE).minWidth(80).text = "閉じる";
+        close.id = "close-button";
         exits.addChild(close);
         _buttonActions = [ACTION_CLOSE];
         addChild(exits);
@@ -259,3 +289,119 @@ auto unzip(string srcFile, string destDir)
     }
 }
 
+class CharacterSettingDialog: Dialog
+{
+    this(Window parent, const Character[dstring] chars, dstring name = "", dstring url = "")
+    {
+        super(UIString("キャラクター設定"d), parent, DialogFlag.Popup);
+        this.name = name;
+        this.url = url;
+        this.chars = chars;
+    }
+
+    override void initialize()
+    {
+        auto wLayout = parseML(q{
+                VerticalLayout {
+                    padding: 10
+                    TableLayout {
+                        colCount: 3
+                        TextWidget {
+                            text: "キャラクター名"
+                        }
+                        EditLine {
+                            id: charNameBox
+                            minWidth: 200
+                        }
+                        TextWidget {
+                            id: "alert"
+                            textColor: "red"
+                        }
+                        TextWidget {
+                            text: "URL"
+                        }
+                        EditLine {
+                            id: urlBox
+                            minWidth: 200
+                            maxWidth: 500
+                        }
+                        HorizontalLayout {
+                            Button {
+                                id: ponButton
+                                text: "スキるぽん"
+                            }
+                        }
+                    }
+                }
+            });
+
+        addChild(wLayout);
+
+        if (!name.empty)
+        {
+            charNameBox.text = name;
+            charNameBox.enabled = false;
+        }
+        charNameBox.contentChange = (EditableContent con) {
+            auto txt = con.text;
+            if (txt.empty)
+            {
+                childById("ok-button").enabled = false;
+                childById("alert").text = "";
+            }
+            else if (chars.keys.canFind(txt))
+            {
+                childById("ok-button").enabled = false;
+                childById("alert").text = "既に存在しています";
+            }
+            else
+            {
+                childById("ok-button").enabled = true;
+                childById("alert").text = "";
+            }
+        };
+
+        urlBox.text = url;
+        urlBox.contentChange = (EditableContent con) {
+            import std.regex;
+            auto txt = con.text;
+            if (charNameBox.enabled && txt.match(ctRegex!r"&%"d))
+            {
+                import std.uri;
+                auto str = txt.matchFirst(ctRegex!r"&(%.+?)$"d)[1];
+                charNameBox.text = str.decode.to!dstring;
+            }
+        };
+
+        childById("ponButton").click = (Widget src) {
+            enum PonURL = "http://www.ponz-web.com/skill/";
+            Platform.instance.openURL(PonURL);
+            return true;
+        };
+
+        auto exits = new HorizontalLayout;
+        auto spacer = new FrameLayout;
+        spacer.minWidth(300);
+        spacer.layoutWidth(FILL_PARENT);
+        exits.addChild(spacer);
+        auto ok = new Button(ACTION_OK).minWidth(80).text = "決定";
+        ok.id = "ok-button";
+        auto cancel = new Button(ACTION_CANCEL).minWidth(80).text = "キャンセル";
+        cancel.id = "cancel-button";
+        exits.addChildren([ok, cancel]);
+        _buttonActions = [ACTION_OK, ACTION_CANCEL];
+        addChild(exits);
+    }
+
+    @property auto charNameBox() {
+        return cast(EditLine)childById("charNameBox");
+    }
+
+    @property auto urlBox() {
+        return cast(EditLine)childById("urlBox");
+    }
+private:
+    dstring name;
+    dstring url;
+    const Character[dstring] chars;
+}

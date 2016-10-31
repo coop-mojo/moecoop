83ffa5564e53225660f1245ca54cf2ae4d85e0b5
もじょんぬ <moe.e.coop@gmail.com>
2016-11-01 07:58:32 +0900

    x64 版のみ migemo.dll もアーカイブに含めるようにした

--- archive.bat
+++ archive.bat
@@ -1,4 +1,9 @@
 @echo off
 dub build -a %1 -b %2
-powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs, migemo.dll, libcurl.dll -DestinationPath moecoop-%1.zip
+
+powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs, libcurl.dll -DestinationPath moecoop-%1.zip
 del fukuro.exe libcurl.dll
+
+if %1 == "x86_64" (
+    powershell -Command Compress-Archive -Path migemo.dll -DestinationPath moecoop-%1.zip -Update
+)

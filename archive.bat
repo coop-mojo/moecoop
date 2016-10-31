fb4cab3c1202013e0bd75eb24a8caecc02391fff
もじょんぬ <moe.e.coop@gmail.com>
2016-11-01 07:44:54 +0900

    アーカイブに migemo.dll も含めるようにした

--- archive.bat
+++ archive.bat
@@ -1,4 +1,4 @@
 @echo off
 dub build -a %1 -b %2
-powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs, libcurl.dll -DestinationPath moecoop-%1.zip
+powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs, migemo.dll, libcurl.dll -DestinationPath moecoop-%1.zip
 del fukuro.exe libcurl.dll

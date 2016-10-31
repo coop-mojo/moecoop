ef04fcff7caea1001e4beebe1c474957fb0c15a6
もじょんぬ <moe.e.coop@gmail.com>
2016-10-31 22:19:17 +0900

    dub のビルド時に、試験的に migemo.dll をダウンロードするようにした

--- archive.bat
+++ archive.bat
@@ -1,4 +1,4 @@
 @echo off
-dub build --combined -a %1 -b %2
+dub build -a %1 -b %2
 powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs, libcurl.dll -DestinationPath moecoop-%1.zip
 del fukuro.exe libcurl.dll

96dc99c2f923842f012a73e7a7f5a40edced8140
もじょんぬ <moe.e.coop@gmail.com>
2016-11-01 21:12:22 +0900

    archive.bat のアーカイブ後の処理を修正

--- archive.bat
+++ archive.bat
@@ -1,9 +1,11 @@
 @echo off
 dub build -a %1 -b %2
 
-powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs, libcurl.dll -DestinationPath moecoop-%1.zip
-del fukuro.exe libcurl.dll
+powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs -DestinationPath moecoop-%1.zip
 
 if %1==x86_64 (
     powershell -Command Compress-Archive -Path migemo.dll -DestinationPath moecoop-%1.zip -Update
+    del migemo.dll
+    rd resource\dict\dict
 )
+del fukuro.exe

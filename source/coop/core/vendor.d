563ec22a21ca0b712eb7773d6bf04ca8627f571d
もじょんぬ <moe.e.coop@gmail.com>
2017-01-16 06:36:45 +0900

    売店情報の読み込みに vibe のシリアライザを使うようにした

--- source/coop/core/vendor.d
+++ source/coop/core/vendor.d
@@ -5,53 +5,33 @@
  */
 module coop.core.vendor;
 
-import std.json;
-import std.typecons;
-
-alias ProductInfo = Tuple!(int, "price", string, "remarks");
-
-struct Vendor{
-    string name;
-    ProductInfo[string] products;
+struct ProductInfo
+{
+    import vibe.data.json;
+    @name("価格") int price;
+    @name("備考") @optional string remarks;
 }
 
-auto readVendors(string fname)
+struct Vendor
 {
-    import std.algorithm;
-    import std.conv;
-    import std.exception;
-    import std.file;
-    import std.json;
-
-    auto res = fname.readText.parseJSON;
-    enforce(res.type == JSON_TYPE.OBJECT);
-    auto vendors = res.object;
-    return vendors.keys.map!(key =>
-                             tuple(key.to!string,
-                                   key.toVendor(vendors[key].object, fname)));
+    import vibe.data.json: name_ = name;
+    @name_("名前") string name;
+    @name_("販売情報") ProductInfo[string] products;
 }
 
 /**
- * 販売員 s の情報を、ファイル fname に書かれている json から読み込む
+ * ファイル fname に含まれる販売員の情報を返す
  */
-auto toVendor(string s, JSONValue[string] json, string fname)
+auto readVendors(string fname)
 {
-    Vendor v;
-    with(v)
-    {
-        import std.algorithm;
-        import std.conv;
-        import std.range;
+    import vibe.data.json;
 
-        import coop.util;
+    import std.algorithm;
+    import std.file;
+    import std.typecons;
 
-        name = s.to!string;
-        products = json.keys
-                       .map!(key =>
-                             tuple(key.to!string,
-                                   ProductInfo(json[key]["価格"].jto!int,
-                                               json[key].object.get("備考", JSONValue("")).jto!string)))
-                       .assocArray;
-    }
-    return v;
+    return fname.readText
+                .parseJsonString
+                .deserialize!(JsonSerializer, Vendor[])
+                .map!"tuple(a.name, a)";
 }

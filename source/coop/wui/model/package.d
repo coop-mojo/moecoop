8248d0dea5e9330bf849100506bae2d442b9ee9a
もじょんぬ <moe.e.coop@gmail.com>
2017-01-26 00:50:17 +0900

    WUI 版を Server 版に名称変更

--- source/coop/wui/model/package.d
+++ source/coop/wui/model/package.d
@@ -3,7 +3,7 @@
  * Authors: Mojo
  * License: $(LINK2 https://github.com/coop-mojo/moecoop/blob/master/LICENSE, MIT License)
  */
-module coop.wui.model;
+module coop.server.model;
 
 interface ModelAPI
 {

e2ee9df4f02ff79ef231205fa6b0fe1c9e788b5f
もじょんぬ <moe.e.coop@gmail.com>
2017-02-09 15:25:27 +0900

    ldc のバージョンアップに伴い、不要な fallback を削除

--- source/coop/fallback.d
+++ source/coop/fallback.d
@@ -7,36 +7,6 @@ module coop.fallback;
 
 version(LDC)
 {
-    // from std.exception
-    mixin template basicExceptionCtors()
-    {
-        /++
-         Params:
-         msg  = The message for the exception.
-         file = The file where the exception occurred.
-         line = The line number where the exception occurred.
-         next = The previous exception in the chain of exceptions, if any.
-         +/
-        this(string msg, string file = __FILE__, size_t line = __LINE__,
-             Throwable next = null) @nogc @safe pure nothrow
-        {
-            super(msg, file, line, next);
-        }
-
-        /++
-         Params:
-         msg  = The message for the exception.
-         next = The previous exception in the chain of exceptions.
-         file = The file where the exception occurred.
-         line = The line number where the exception occurred.
-         +/
-        this(string msg, Throwable next, string file = __FILE__,
-             size_t line = __LINE__) @nogc @safe pure nothrow
-        {
-            super(msg, file, line, next);
-        }
-    }
-
     // from std.algorithm.iteration
     template fold(fun...) if (fun.length >= 1)
     {

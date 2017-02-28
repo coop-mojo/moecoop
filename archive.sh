d7c259ed1bcbf1338b05196a3053fca313925e46
もじょんぬ <moe.e.coop@gmail.com>
2017-02-28 21:56:02 +0900

    archive.sh で、不要な依存関係を手動で削除するようにした

--- archive.sh
+++ archive.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+dub upgrade
+sed -i -e 's/^.\+openssl.\+$//' dub.selections.json
 dub build -c server -b release
 strip fukurod
 tar cvzf moecoop.tgz fukurod LICENSE README.md resource docs

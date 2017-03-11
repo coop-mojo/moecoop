0c32183196369ced9333b826a8a1dbb15da3a3e2
もじょんぬ <moe.e.coop@gmail.com>
2017-03-11 19:00:29 +0900

    [AppVeyor] OpenSSL 用のライブラリをアーカイブに追加

--- archive.bat
+++ archive.bat
@@ -3,7 +3,7 @@
 dub upgrade
 dub build -c mui -a %1 -b %2
 
-powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs -DestinationPath moecoop-%1.zip
+powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs, libeay32.dll, ssleay32.dll -DestinationPath moecoop-%1.zip
 
 if %1==x86_64 (
     powershell -Command Compress-Archive -Path migemo.dll -DestinationPath moecoop-%1.zip -Update

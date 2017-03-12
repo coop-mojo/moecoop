246755c610e7d2bfaf900bb460d2cdd457683a86
もじょんぬ <moe.e.coop@gmail.com>
2017-03-12 12:03:31 +0900

    [MUI] Win32 環境に libevent.dll を添付するようにした

--- archive.bat
+++ archive.bat
@@ -4,4 +4,9 @@ dub upgrade
 dub build -c mui -a %1 -b %2
 
 powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, docs, libeay32.dll, ssleay32.dll -DestinationPath moecoop-%1.zip
+
+if %1==x86 (
+    powershell -Command Compress-Archive -Path libevent.dll -DestinationPath moecoop-%1.zip -Update
+)
+
 del fukuro.exe

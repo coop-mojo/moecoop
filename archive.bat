fcf362680dfcaa3e3faaa5a6bd18d77be0e4f559
もじょんぬ <moe.e.coop@gmail.com>
2017-03-12 11:00:35 +0900

    [MUI] Windows 版に migemo や元データを含めないようにした

--- archive.bat
+++ archive.bat
@@ -3,11 +3,5 @@
 dub upgrade
 dub build -c mui -a %1 -b %2
 
-powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, resource, docs, libeay32.dll, ssleay32.dll -DestinationPath moecoop-%1.zip
-
-if %1==x86_64 (
-    powershell -Command Compress-Archive -Path migemo.dll -DestinationPath moecoop-%1.zip -Update
-    del migemo.dll
-    rd resource\dict\dict
-)
+powershell -Command Compress-Archive -Path fukuro.exe, LICENSE, README.md, docs, libeay32.dll, ssleay32.dll -DestinationPath moecoop-%1.zip
 del fukuro.exe

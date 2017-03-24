955306e358a0592d434c4e16866760352318cf07
もじょんぬ <moe.e.coop@gmail.com>
2017-03-24 21:36:16 +0900

    サーバー部分のコードを削除

--- archive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-dub upgrade
-sed -i -e 's/^.\+openssl.\+$//' dub.selections.json
-dub build -c server -b release
-strip fukurod
-tar cvzf moecoop.tgz fukurod LICENSE README.md resource docs
-rm fukurod
